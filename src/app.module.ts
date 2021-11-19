0b902cc51a4108cc50328038706caa3963c23597
alexlau <lauyy@umich.edu>
2021-11-19 11:39:36 +0800

    Created services and classes for Pair, API endpoint now returns a list of pairs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PairsController } from './pairs/pairs.controller';
+import { PairsService } from './pairs/pairs.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PairsController],
+  providers: [AppService, PairsService],
 })
 export class AppModule {}
