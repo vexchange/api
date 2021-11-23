8eed917520144a1b66ee2f695e8b0a1a8f3a77ff
alexlau <lauyy@umich.edu>
2021-11-23 15:23:23 +0800

    Added throttler for rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,28 @@ import { TokenController } from '@controllers/token.controller';
 import { PairsController } from '@controllers/pairs.controller';
 import { OnchainDataService } from '@services/onchain-data.service';
 import { CoinGeckoService } from '@services/coin-gecko.service';
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
+import { APP_GUARD } from "@nestjs/core";
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot({
+      // This means that the user can only make
+      // 10 queries per 60 seconds
+      // i.e. 1 query per 6 seconds
+      ttl: 60,
+      limit: 10,
+    }),
+  ],
   controllers: [PairsController, TokenController],
-  providers: [OnchainDataService, CoinGeckoService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+    OnchainDataService,
+    CoinGeckoService,
+  ],
   exports: [],
 })
 export class AppModule {}
