e85b8a4ec758c0b62e64e190b78bd062d8f15b3e
Anderson Aguiar <revsoft10@gmail.com>
2021-11-24 15:15:45 +1100

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { fastifyHelmet } from 'fastify-helmet';
 import {
   FastifyAdapter,
   NestFastifyApplication,
@@ -12,11 +14,23 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
 
+  const config = new DocumentBuilder()
+    .setTitle('Vexchange API')
+    .setVersion('1.0')
+    .addServer('v1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   app.enableVersioning({
     type: VersioningType.URI,
   });
 
   app.enableShutdownHooks();
+  await app.register(fastifyHelmet, {
+    contentSecurityPolicy: false,
+  });
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
