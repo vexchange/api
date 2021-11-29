ca6af4c2522036e67111db8717f218bee8b5fab7
Anderson Aguiar <revsoft10@gmail.com>
2021-11-29 14:55:08 +1100

    Add tests

---
+++ test/controllers/pair.controller.spec.ts
@@ -0,0 +1,72 @@
+import { PairController } from '../../src/controllers/pair.controller';
+import { OnchainDataService } from '@services/onchain-data.service';
+import { Test } from '@nestjs/testing';
+import { allPairs, pair } from '../mocks/pair';
+import { CoinGeckoService } from '@services/coin-gecko.service';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+
+describe('PairController', () => {
+  let pairController: PairController;
+  let onchainDataService: OnchainDataService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      controllers: [PairController],
+      providers: [CoinGeckoService, OnchainDataService],
+    }).compile();
+
+    onchainDataService = moduleRef.get<OnchainDataService>(OnchainDataService);
+    pairController = moduleRef.get<PairController>(PairController);
+  });
+
+  describe('getPairs', () => {
+    it('should return all pairs', async () => {
+      jest
+        .spyOn(onchainDataService, 'getAllPairs')
+        .mockImplementation(() => allPairs);
+
+      expect(pairController.getPairs()).toBe(allPairs);
+    });
+  });
+
+  describe('getPairFromAddress', () => {
+    it('should return pair', async () => {
+      // arrange
+      const address = '0x2b6fc877ff5535b50f6c3e068bb436b16ec76fc5';
+      jest.spyOn(onchainDataService, 'getPair').mockImplementation(() => pair);
+
+      // act & assert
+      expect(pairController.getPairFromAddress(address)).toBe(pair);
+    });
+
+    it('should throw exception - Pair does not exist', async () => {
+      // arrange
+      const address = '0x2b6fc877ff5535b50f6c3e068bb436b16ec76fc5';
+      jest.spyOn(onchainDataService, 'getPair').mockImplementation(() => null);
+
+      try {
+        // act
+        pairController.getPairFromAddress(address);
+      } catch (error) {
+        // assert
+        expect(error).toStrictEqual(
+          new NotFoundException('Pair does not exist'),
+        );
+      }
+    });
+
+    it('should throw exception - Invalid request', async () => {
+      // arrange
+      const address = null;
+      jest.spyOn(onchainDataService, 'getPair').mockImplementation(() => null);
+
+      try {
+        // act
+        pairController.getPairFromAddress(address);
+      } catch (error) {
+        // assert
+        expect(error).toStrictEqual(new BadRequestException('Invalid request'));
+      }
+    });
+  });
+});
---
+++ test/controllers/token.controller.spec.ts
@@ -0,0 +1,74 @@
+import { TokenController } from '../../src/controllers/token.controller';
+import { OnchainDataService } from '@services/onchain-data.service';
+import { Test } from '@nestjs/testing';
+import { CoinGeckoService } from '@services/coin-gecko.service';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { allTokens, token } from '../mocks/token';
+
+describe('TokenController', () => {
+  let tokenController: TokenController;
+  let onchainDataService: OnchainDataService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      controllers: [TokenController],
+      providers: [CoinGeckoService, OnchainDataService],
+    }).compile();
+
+    onchainDataService = moduleRef.get<OnchainDataService>(OnchainDataService);
+    tokenController = moduleRef.get<TokenController>(TokenController);
+  });
+
+  describe('getTokens', () => {
+    it('should return all tokens', async () => {
+      jest
+        .spyOn(onchainDataService, 'getAllTokens')
+        .mockImplementation(() => allTokens);
+
+      expect(tokenController.getTokens()).toBe(allTokens);
+    });
+  });
+
+  describe('getTokenFromAddress', () => {
+    it('should return token', async () => {
+      // arrange
+      const address = '0x2b6fc877ff5535b50f6c3e068bb436b16ec76fc5';
+      jest
+        .spyOn(onchainDataService, 'getToken')
+        .mockImplementation(() => token);
+
+      // act & assert
+      expect(tokenController.getTokenFromAddress(address)).toBe(token);
+    });
+
+    it('should throw exception - Token does not exist', async () => {
+      // arrange
+      const address = '0x2b6fc877ff5535b50f6c3e068bb436b16ec76fc5';
+      jest.spyOn(onchainDataService, 'getToken').mockImplementation(() => null);
+
+      try {
+        // act
+        tokenController.getTokenFromAddress(address);
+      } catch (error) {
+        // assert
+        expect(error).toStrictEqual(
+          new NotFoundException('Token does not exist'),
+        );
+      }
+    });
+
+    it('should throw exception - Invalid request', async () => {
+      // arrange
+      const address = null;
+      jest.spyOn(onchainDataService, 'getToken').mockImplementation(() => null);
+
+      try {
+        // act
+        tokenController.getTokenFromAddress(address);
+      } catch (error) {
+        // assert
+        expect(error).toStrictEqual(new BadRequestException('Invalid request'));
+      }
+    });
+  });
+});
