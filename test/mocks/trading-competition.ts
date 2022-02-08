4d5846d417c1a1b6a311c48aa22a49c1b0277b7c
Anderson Aguiar <revsoft10@gmail.com>
2022-02-08 14:28:01 -0300

    Add controller test

---
+++ test/mocks/trading-competition.ts
@@ -0,0 +1,17 @@
+export const fullRanking = [
+  {
+      "address": "0x6c0a6e1d922e0e63901301573370b932ae20dadb",
+      "points": "9503623.279943905630866475",
+      "rank": 1
+  },
+  {
+      "address": "0x955b48a46698b2b8330d75dc88ae5b95cd7ff9f4",
+      "points": "34830.877811112279293794",
+      "rank": 2
+  },
+  {
+      "address": "0x5837dbaae7a739fbc264737920512efdf00af1ef",
+      "points": "28584.151805726608600402",
+      "rank": 3
+  }
+]
