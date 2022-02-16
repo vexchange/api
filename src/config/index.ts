eed9822a31cea0a0d83d4ee520bcf885b6f1420d
Anderson Aguiar <revsoft10@gmail.com>
2022-02-16 00:32:15 -0300

    Add config values

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
 export const config = (): object => ({
     port: process.env.PORT,
     tradingCompetition: {
-        pairAddress: process.env.TRADING_COMPETITION_PAIR_ADDRESS,
-        fromBlock: Number(process.env.TRADING_COMPETITION_FROM_BLOCK),
-        toBlock: Number(process.env.TRADING_COMPETITION_TO_BLOCK),
+        pairAddress: '0x39cd888a1583498AD30E716625AE1a00ff51286D',
+        fromBlock: Number(10848841),
+        toBlock: Number(11401689),
     },
 });
